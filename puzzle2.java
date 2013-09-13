1fc10f249b03b80dc86d79bd45c9faf2559788e6
kaushalsmile <kaushalshah.csis@gmail.com>
2013-09-13 23:38:44 +0530

    Create puzzle2.java
    Assumption :
    Given input is valid input.
    Cups capacity and level should be positive number.

---
+++ puzzle2.java
@@ -0,0 +1,42 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Puzzle2 {
+    public void print() {
+        NumberFormat nf = NumberFormat.getInstance();
+        nf.setMaximumFractionDigits(2);
+        nf.setMinimumFractionDigits(0);
+        Scanner input = new Scanner(System.in);
+        int level = input.nextInt();
+        int cnt = 0;
+        double[] cups = new double[(level * (level + 1)) / 2];
+
+        for (int i = 0; i < cups.length; i++) {
+            cups[i] = input.nextDouble();
+        }
+        double extraWater = input.nextDouble();
+        double tmp = 0;
+        for (int i = 0; i < level - 1; i++) {
+            tmp = extraWater / (i + 1);
+            extraWater = 0;
+            for (int j = 0; j <= i; j++) {
+                if (cups[i + j] < tmp) {
+                    extraWater += tmp - cups[cnt];
+                } else {
+                    cups[cnt] = tmp;
+                }
+                System.out.print(nf.format(cups[cnt]) + " ");
+                cnt ++;
+            }System.out.println();
+        }
+        tmp = extraWater / level;
+        for (int i = 0; i < level; i++) {
+            System.out.print( nf.format(tmp)+ " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        new Puzzle2().print();
+    }
+
+}
